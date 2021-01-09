0b19be18e42b4569e49a933387a40c12cf793d3e
LeonelFerreira <37122@ufp.edu.pt>
2021-01-09 18:40:14 +0000

    Testes
    Testes Controladores Projecto e Cliente
    Update Projecto

--- src/main/java/projecto/controller/ProjetoControlador.java
+++ src/main/java/projecto/controller/ProjetoControlador.java
@@ -43,6 +43,13 @@ public class ProjetoControlador {
         return ResponseEntity.created(uri).build();
     }
 
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Void> update(@Valid @RequestBody Projeto obj, @PathVariable Integer id) {
+        obj.setId(id);
+        obj = projetoService.update(obj);
+        return ResponseEntity.noContent().build();
+    }
+
     @RequestMapping(value="/{id}", method=RequestMethod.DELETE)
     public ResponseEntity<Void> delete(@PathVariable Integer id) {
         projetoService.delete(id);
