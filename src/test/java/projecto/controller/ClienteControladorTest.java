de95ee094e3d1e127263a5d27295cec1076d3f65
LeonelFerreira <37122@ufp.edu.pt>
2021-01-12 22:47:22 +0000

    Testes
    Testesdos controladores de clientes e colaboradores

--- src/test/java/projecto/controller/ClienteControladorTest.java
+++ src/test/java/projecto/controller/ClienteControladorTest.java
@@ -1,67 +1,102 @@
 package projecto.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import projecto.repositories.ClienteRepository;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 import projecto.model.Cliente;
+import projecto.service.ClienteService;
+
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@DataJpaTest
+
+@WebMvcTest(ClienteControlador.class)
 class ClienteControladorTest {
 
     @Autowired
-    private ClienteRepository clienteRepository;
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ClienteService clienteService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Test
-    void find() {
-        insert();
-        Optional<Cliente> cliente = clienteRepository.findById(1);
-        assertEquals(1, cliente.get().getId());
+    void find() throws Exception {
+
+        Cliente cliente=new Cliente();
+        String clienteAsJsonString=new ObjectMapper().writeValueAsString(cliente);
+
+        when(clienteService.find(1)).thenReturn(cliente);
+
+        String httpResponseAsString=mockMvc.perform(get("/cliente/1")).andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
+        assertNotNull(httpResponseAsString);
+
+        mockMvc.perform(get("/cliente/2")).andExpect(status().isNotFound());
     }
 
     @Test
-    void findAll() {
-        insert();
-        assertEquals(6, clienteRepository.count());
+    void findAll() throws Exception {
 
-        List<Cliente> clientes = clienteRepository.findAll();
+        Cliente cliente1=new Cliente();
+        Cliente cliente2=new Cliente();
+        Cliente cliente3=new Cliente();
 
-        assertEquals(6, clientes.size());
+        List<Cliente> clientes= Arrays.asList(cliente1,cliente2,cliente3);
+
+        when(clienteService.findAll()).thenReturn(clientes);
+
+        String httpResponseAsString=mockMvc.perform(get("/cliente")).andDo(print()).andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
+        assertNotNull(httpResponseAsString);
+
     }
 
+
     @Test
-    void insert() {
-        Cliente cliente = new Cliente("Rui", "123456789");
-        assertEquals(0, clienteRepository.count());
-        clienteRepository.save(cliente);
-        assertEquals(1, clienteRepository.count());
+    void insert() throws Exception {
+        Cliente cliente=new Cliente();
+        cliente.setNome("Novo Cliente");
+        cliente.setNif("123456789");
+
+        when(this.clienteService.insert(cliente)).thenReturn(cliente);
+
+        String clienteAsJsonString=new ObjectMapper().writeValueAsString(cliente);
+
+        mockMvc.perform(post("/cliente").content(clienteAsJsonString).contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
+
+        Cliente clienteExistente=new Cliente();
+        clienteExistente.setNome("Novo Cliente");
+        clienteExistente.setNif("123456789");
+        String clienteExistenteAsJsonString=new ObjectMapper().writeValueAsString(clienteExistente);
+
+        mockMvc.perform(post("/cliente").content(clienteExistenteAsJsonString).contentType(MediaType.APPLICATION_JSON)).andExpect(status().isBadRequest());
     }
 
     @Test
     void update() {
-        insert();
-        Optional<Cliente> cli1 = clienteRepository.findById(6);
-        assertEquals("123456789", cli1.get().getNif());
 
-        cli1.get().setNif("987654321");
-        clienteRepository.save(cli1.get());
 
-        Optional<Cliente> cli2 = clienteRepository.findById(6);
-        assertEquals("987654321", cli2.get().getNif());
+
 
     }
 
     @Test
     void delete() {
-        insert();
-        assertEquals(6, clienteRepository.count());
-        clienteRepository.delete(clienteRepository.getOne(1));
 
-        assertEquals(5, clienteRepository.count());
     }
 
     @Test
