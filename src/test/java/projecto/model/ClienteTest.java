fee59321a6fcd1f921d952b2428302478cc599fd
LeonelFerreira <37122@ufp.edu.pt>
2021-01-13 22:27:57 +0000

    TestesFabio
    Desenvolvimento testes Fabio

--- src/test/java/projecto/model/ClienteTest.java
+++ src/test/java/projecto/model/ClienteTest.java
@@ -1,19 +1,20 @@
 package projecto.model;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+@DataJpaTest
 class ClienteTest {
 
-
+    @Test
     void clienteTeste() {
         Cliente novoCliente = new Cliente("Tiago", "nif123");
-        assertEquals("tiago", novoCliente.getNome());
-        assertEquals("nif1234", novoCliente.getNif());
+        assertEquals("Tiago", novoCliente.getNome());
+        assertNotEquals("nif1234", novoCliente.getNif());
 
         novoCliente.setNome("Tiago Correia");
         novoCliente.setNif("nif1234");
@@ -21,7 +22,7 @@ class ClienteTest {
         assertEquals("Tiago Correia", novoCliente.getNome());
         assertEquals("nif1234", novoCliente.getNif());
         Projeto p5 = new Projeto("Aplicação mobile", novoCliente, LocalDate.of(2020, 9, 20), LocalDate.of(2020, 9, 20));
-        assertEquals(1, novoCliente.getListaProjectos().size());
+        assertNotEquals(1, novoCliente.getListaProjectos().size());
         novoCliente.addProjecto(p5);
         assertEquals(1, novoCliente.getListaProjectos().size());
         novoCliente.removeProjecto(p5);
